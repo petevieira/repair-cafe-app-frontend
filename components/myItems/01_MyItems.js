936375d156e0763350379ff9dbe73d9286193bdb
Lauren Dahlin <ldahlin@rhsmith.umd.edu>
2022-12-28 22:05:34 -0700

    Working nested maps on My Items page

--- components/myItems/01_MyItems.js
+++ components/myItems/01_MyItems.js
@@ -19,59 +19,12 @@ import styles from '../../globals/Styles.js'
 // Fake data
 import fakeUserEventsItems from '../../images/example_user_events.json';
 
+fakeUserEventsItems.sort((a, b) => (new Date(b.startDatetime)).getTime() - (new Date(a.startDatetime)).getTime());
+
 const MyItems = () => {
 
   // Today's date
   const today = format(new Date(), "MMMM do, yyyy");
-
-  // Date formatting
-  const DateString = (eventDate) =>  {
-
-    const date = new Date(eventDate);
-    const formattedDate = format(date, "MMMM do, yyyy");
-    return formattedDate;
-
-  }
-
-  // Number of events the user has
-  const numEvents = fakeUserEventsItems.length;
-
-  // Use event 3 as an example
-  const eventIndex = 2;
-  const activeEvent = fakeUserEventsItems[eventIndex].activeEvent;
-  const location = fakeUserEventsItems[eventIndex].locationName;
-  const eventStartDate = DateString(fakeUserEventsItems[eventIndex].startDatetime);
-
-  // If the event date is today, display "Today's Event",
-  // If event is active but not today, display "Next Event"
-  let eventLine1 = "";
-  if (activeEvent == true && eventStartDate == today) {
-    eventLine1 = `Today's Event: ${location}`;
-  } else if (activeEvent == true && eventStartDate != today) {
-    eventLine1 = `Next Event: ${location}`;
-  } else {
-    eventLine1 = `${location}`;
-  }
-
-  // Item info for testing
-  const itemName = fakeUserEventsItems[eventIndex].items[0].itemType.name;
-  const itemImage = fakeUserEventsItems[eventIndex].items[0].itemType.imageUrl;
-
-  // Status
-  const rawItemStatus = fakeUserEventsItems[eventIndex].items[0].status;
-  const itemRepairerName = fakeUserEventsItems[eventIndex].items[0].repairerName;
-  let itemStatus = ""
-  let viewRepairLog = "Repair log available when complete"
-  if (rawItemStatus == "Waiting on Assignment"){
-    itemStatus = rawItemStatus;
-  } else if (rawItemStatus == "In Progress") {
-    itemStatus = `Assigned to ${itemRepairerName}`;
-  } else if (rawItemStatus == "Complete") {
-    itemStatus = `Completed by ${itemRepairerName}`;
-    viewRepairLog = "View Repair Log\nThank Repairer";
-  } else {
-    itemStatus = "Unknown";
-  }
 
   return (
     <View style={styles.container}>
@@ -81,34 +34,41 @@ const MyItems = () => {
 
       <View style = {{alignItems: 'flex-start', justifyContent: 'center', flex: 1, margin: 20}}>
         <ScrollView>
+          {fakeUserEventsItems.map((event)=> {
 
-          <Text>
-            <Text style = {{ fontWeight: 'bold', fontSize: 16}}>{eventLine1}{"\n"}</Text>
-            {eventStartDate}
-          </Text>
+            const eventStartDate = format(new Date(event.startDatetime), "MMMM do, yyyy");
+            const activeEvent = event.activeEvent;
+            const location = event.locationName;
 
-          <Divider style={{ height: 2 }}/>
 
-          <View style = {{margin: 5}}>
-            <Button icon = "plus" mode = "elevated" textColor = "black">
-            Add Item
-            </Button>
-          </View>
+            let eventLine1 = "";
+            if (activeEvent == true && eventStartDate == today) {
+              eventLine1 = `Today's Event: ${location}`;
+            } else if (activeEvent == true && eventStartDate != today) {
+              eventLine1 = `Next Event: ${location}`;
+            } else {
+              eventLine1 = `Past Event: ${location}`;
+            }
 
-          <View style = {{borderWidth: 1, padding: 10, flex: 1, flexDirection: 'row'}}>
-           <Image source={{ uri: itemImage }} style={styles.itemImage} />
-           <View style = {{paddingLeft: 5, paddingRight: 5}}>
-            <Text style = {{marginBottom: 3}}>
-              {itemName.toUpperCase()}{"\n"}
-              Status: {itemStatus}{"\n"}
-              {viewRepairLog}
-            </Text>
-              <Button mode = "outlined" compact = "true" dark = "true" uppercase = "false">
-              QR Code for Repairer
-              </Button>
-           </View>
-          </View>
+            return (
+              <View key={event.EventId}>
 
+                <Text>
+                <Text style = {{ fontWeight: 'bold', fontSize: 16}}>{eventLine1}{"\n"}</Text>
+                {eventStartDate}
+                </Text>
+
+                <Divider style={{ height: 2 }}/>
+
+                {event.items.map((item)=> {
+                  return (
+                    <Text key={item.itemId}>{item.itemType.name}</Text>
+                  )
+                })}
+
+              </View>
+            )
+          })}
         </ScrollView>
       </View>
     </View>
